c41947e8de9aea4a342535db1e90ac944772d151
Nazmus08Sakib <sakibnazmus97@gmail.com>
2025-04-07 22:21:02 +0600

    basic setup, shadcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>welcome to our course</h1>
+    <div className="p-5 relative">
+      <div className="w-100 flex justify-end absolute top-5 right-5">
+        <ThemeToggle />
+      </div>
+      <h1 className="fon-bold text-blue-500 font-barlow">welcome to our course</h1>
+      <Button variant="destructive">click me</Button>
     </div>
   );
 }
